00d27aefee499b3ca4994fe657ed605395037216
Syam Kumar <yadavallisyam1@gmail.com>
2021-06-20 18:56:22 +0530

    first commit

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -8,12 +8,12 @@ const Form = (props) => {
         <div className="page form-page">
             <div className="page-container formpage-container">
                 <div className="formcard-container">
-                    <div className={styles.card}>
-                        <div className="form-intro">
-                            <h2 className={styles.header}>{props.header}</h2>
-                            <p className={styles.description}>{props.description}</p>
-                        </div>
+
+                    <div className="form-intro">
+                        <h2 className={styles.header}>{props.header}</h2>
+                        <p className={styles.description}>{props.description}</p>
                     </div>
+
                     <div className="form">
                         <FormCard />
                     </div>
